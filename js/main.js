e9f1914b3f3ccf1400264cb9fb23bf2e205e8a0a
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-07-27 13:58:57 -0400

    feat: Connect modal to add to cart button and encase in <form> tags

--- js/main.js
+++ js/main.js
@@ -4,6 +4,35 @@ function changeImage(imgName)
   image.src = imgName;
   }
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
 // var button1 = document.getElementById("example-one");
 // var button2 = document.getElementById("example-two");
 // var button3 = document.getElementById("example-three");
