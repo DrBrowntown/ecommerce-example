4c4ca1a3d0d9d72d38d00d9a58efd76a7cb78144
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-07-27 11:23:11 -0400

    feat: Add text toggle for expanding and collapsing details

--- js/main.js
+++ js/main.js
@@ -3,3 +3,40 @@ function changeImage(imgName)
   image = document.getElementById('imgDisp');
   image.src = imgName;
   }
+
+// var button1 = document.getElementById("example-one");
+// var button2 = document.getElementById("example-two");
+// var button3 = document.getElementById("example-three");
+
+
+
+// button1.addEventListener('click', function() {
+//   if (button1.getAttribute("data-text-swap") == button1.innerHTML) {
+//     button1.innerHTML = button1.getAttribute("data-text-original");
+//   } else {
+//     button1.setAttribute("data-text-original", button1.innerHTML);
+//     button1.innerHTML = button1.getAttribute("data-text-swap");
+//   }
+// }, false);
+
+// button2.addEventListener('click', function() {
+//   if (button2.getAttribute("data-text-swap") == button2.innerHTML) {
+//     button2.innerHTML = button2.getAttribute("data-text-original");
+//   } else {
+//     button2.setAttribute("data-text-original", button2.innerHTML);
+//     button2.innerHTML = button2.getAttribute("data-text-swap");
+//   }
+// }, false);
+
+// button3.addEventListener('click', function() {
+//   if (button3.getAttribute("data-text-swap") == button3.innerHTML) {
+//     button3.innerHTML = button3.getAttribute("data-text-original");
+//   } else {
+//     button3.setAttribute("data-text-original", button3.innerHTML);
+//     button3.innerHTML = button3.getAttribute("data-text-swap");
+//   }
+// }, false);
+
+// function brown(button) {
+
+// }
